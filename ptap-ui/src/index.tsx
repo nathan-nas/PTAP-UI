bb12553e5a6d72e83f109cbc53ebe96477b6129a
AnhNG <77968170+nathan-nas@users.noreply.github.com>
2025-01-20 22:08:09 +0700

    migrate to vite

--- ptap-ui/src/index.tsx
+++ ptap-ui/src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 
 export { Button } from './Button';
 export { Input } from './Input'; 
